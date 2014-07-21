37711cc1d4de3b99fe0d480647d49aace7dd03d2
Josh Hannan <joshh@aycmedia.com>
2014-07-21 16:26:36 -0400

    avoiding duplicate content

--- overlay_it.js
+++ overlay_it.js
@@ -9,16 +9,16 @@
 		$(this).each(function() {
 			if( !$('.overlay_it_container').length ) {
 				$(this).prepend('<div class="overlay_it_container"><div class="overlay_it_box">'+settings.html+'<div class="close">'+settings.close_html+'</div></div></div>');
-				$('.overlay_it_box .close').click(function() {
-					$('.overlay_it_container').remove();
-				});
-				$('.overlay_it_container .overlay_it_box').click(function(e) {
-					e.stopPropagation();
-				});
-				$('.overlay_it_container').click(function() {
-					$('.overlay_it_container').remove();
-				});
 			}
+			$('.overlay_it_box .close').click(function() {
+				$('.overlay_it_container').hide();
+			});
+			$('.overlay_it_container .overlay_it_box').click(function(e) {
+				e.stopPropagation();
+			});
+			$('.overlay_it_container').click(function() {
+				$('.overlay_it_container').hide();
+			});
 		});
 		return this;
 	}; //  END OVERLAY_IT FUNCTION
